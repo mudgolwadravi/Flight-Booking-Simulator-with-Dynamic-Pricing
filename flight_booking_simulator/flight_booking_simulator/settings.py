40c81c9ed198245c0743cbd3edcf6ab9a8a4a05b
mudgolwadravi <mudgolwadravi@gmail.com>
2025-10-27 14:56:06 +0530

    Add Render domain to ALLOWED_HOSTS

--- flight_booking_simulator/flight_booking_simulator/settings.py
+++ flight_booking_simulator/flight_booking_simulator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b(0j#pz*+b%)yh$vl0fj-ak%qcg#ph=&u&0b^pm*8tv3hwkr43
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['flight-booking-simulator-with-dynamic-4m8q.onrender.com']
 
 
 # Application definition
