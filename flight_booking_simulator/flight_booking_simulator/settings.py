aa8cbaa6ad7ec179908863bb046e50ee7f676f69
mudgolwadravi <mudgolwadravi@gmail.com>
2025-10-27 15:47:33 +0530

    Fix ALLOWED_HOSTS for Render domainn

--- flight_booking_simulator/flight_booking_simulator/settings.py
+++ flight_booking_simulator/flight_booking_simulator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b(0j#pz*+b%)yh$vl0fj-ak%qcg#ph=&u&0b^pm*8tv3hwkr43
 DEBUG = True
 
 # ALLOWED_HOSTS = ['flight-booking-simulator-with-dynamic-4m8q.onrender.com']
-ALLOWED_HOSTS = ['flight-booking-simulator-with-dynamic-4m8q.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 
